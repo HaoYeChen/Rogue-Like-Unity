bf177d047922301b2e8a3f67f6925c80c0ca57cc
Hao <54834780+HaoYeChen@users.noreply.github.com>
2020-01-23 08:16:19 +0100

    Rogue Like generation

---
+++ Roguelike/Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    public Transform[] startingPositions;
+    public GameObject[] rooms; //index 0-->closed, index 1-->LR, index 2-->LRB, index 3-->LRT, index 4-->LRBT
+
+    private int direction;
+    public float moveAmount;
+
+    private float timeBtwRoom;
+    public float startTimeBtwRoom = 0.25f;
+
+    //remember to set borders and the reset to the right X and Y in inspector
+    public float minX;
+    public float maxX;
+    public float minY;
+    private bool stopGeneration;
+
+    public LayerMask room;
+    private void Start()
+    {
+        int randStartingPos = Random.Range(0, startingPositions.Length);
+        transform.position = startingPositions[randStartingPos].position;
+        Instantiate(rooms[0], transform.position, Quaternion.identity);
+
+        direction = Random.Range(1, 6);
+    }
+
+    private void Update()
+    {
+        if (timeBtwRoom <= 0 && stopGeneration == false)
+        {
+            Move();
+            timeBtwRoom = startTimeBtwRoom;
+        }
+        else
+        {
+            timeBtwRoom -= Time.deltaTime;
+        }
+    }
+
+    private void Move()
+    {
+        if (direction == 1 || direction == 2)   //Move Right
+        {
+            if (transform.position.x < maxX)
+            {
+                Vector2 newPos = new Vector2(transform.position.x + moveAmount, transform.position.y);
+                transform.position = newPos;
+
+                int rand = Random.Range(0, rooms.Length);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+
+                direction = Random.Range(1, 6);
+                if (direction == 3)
+                {
+                    direction = 2;
+                }
+                else if (direction == 4)
+                {
+                    direction = 5;
+                }
+            }
+            else
+            {
+                direction = 5;
+            }
+            
+        }
+        else if (direction == 3 || direction == 4)  //Move Left
+        {
+            if (transform.position.x > minX)
+            {
+                Vector2 newPos = new Vector2(transform.position.x - moveAmount, transform.position.y);
+                transform.position = newPos;
+
+                int rand = Random.Range(0, rooms.Length);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+
+                direction = Random.Range(3, 6);
+            }
+            else
+            {
+                direction = 5;
+            }
+            
+        }
+        else if (direction == 5)    //Move Down
+        {
+            if (transform.position.y > minY)
+            {
+                Collider2D roomDetection = Physics2D.OverlapCircle(transform.position, 1, room); // Remember to set Layer to room in inspector or it wont work
+                if (roomDetection.GetComponent<RoomType>().type != 1 && roomDetection.GetComponent<RoomType>().type != 3)
+                {
+                    roomDetection.GetComponent<RoomType>().RoomDestruction();
+
+                    int randBottomRoom = Random.Range(1, 4);
+                    if (randBottomRoom == 2)
+                    {
+                        randBottomRoom = 1;
+                    }
+                    Instantiate(rooms[randBottomRoom], transform.position, Quaternion.identity);
+                }
+
+
+                Vector2 newPos = new Vector2(transform.position.x, transform.position.y - moveAmount);
+                transform.position = newPos;
+
+                int rand = Random.Range(2, 4);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+
+                direction = Random.Range(1, 6);
+            }
+            else //Stop Level Generation 
+            {
+                stopGeneration = true;
+            }
+            
+        }
+
+    }
+
+}
